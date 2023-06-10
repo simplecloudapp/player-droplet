0b5fadf863161fb2f46db7b401b099f88dfeb84e
Philipp <p.eistrach@gmail.com>
2023-06-10 19:43:29 +0200

    feat: gRPC server

--- player-server/src/main/kotlin/app/simplecloud/droplet/player/server/PlayerServer.kt
+++ player-server/src/main/kotlin/app/simplecloud/droplet/player/server/PlayerServer.kt
@@ -1,13 +1,27 @@
 package app.simplecloud.droplet.player.server
 
+import io.grpc.ServerBuilder
 import org.apache.logging.log4j.LogManager
+import kotlin.concurrent.thread
 
 class PlayerServer {
 
     private val logger = LogManager.getLogger(PlayerServer::class.java)
 
+    private val server = ServerBuilder.forPort(5816)
+        .build()
+
     fun start() {
-        logger.info("Starting PlayerServer...")
+        logger.info("Starting Player server...")
+        startGrpcServer()
+    }
+
+    private fun startGrpcServer() {
+        logger.info("Starting gRPC server...")
+        thread {
+            server.start()
+            server.awaitTermination()
+        }
     }
 
 }
