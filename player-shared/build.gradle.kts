4386188865b27f9f2560126d950a9b80a0bb2198
Philipp <p.eistrach@gmail.com>
2023-06-10 19:31:32 +0200

    feat: protobuf gradle configuration

---
+++ player-shared/build.gradle.kts
@@ -0,0 +1,41 @@
+import com.google.protobuf.gradle.*
+
+plugins {
+    id("com.google.protobuf") version "0.8.19"
+}
+
+dependencies {
+    api("com.google.protobuf:protobuf-java:3.16.3")
+    api("io.grpc:grpc-stub:1.55.1")
+    api("io.grpc:grpc-protobuf:1.55.1")
+    api("io.grpc:grpc-netty-shaded:1.55.1")
+    api("redis.clients:jedis:4.4.1")
+    api("com.google.code.gson:gson:2.10.1")
+    api("javax.annotation:javax.annotation-api:1.3.2")
+}
+
+protobuf {
+    protoc {
+        // The artifact spec for the Protobuf Compiler
+        artifact = "com.google.protobuf:protoc:3.23.1"
+    }
+    plugins {
+        // Optional: an artifact spec for a protoc plugin, with "grpc" as
+        // the identifier, which can be referred to in the "plugins"
+        // container of the "generateProtoTasks" closure.
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.55.1"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                // Apply the "grpc" plugin whose spec is defined above, without
+                // options. Note the braces cannot be omitted, otherwise the
+                // plugin will not be added. This is because of the implicit way
+                // NamedDomainObjectContainer binds the methods.
+                id("grpc") { }
+            }
+        }
+    }
+}
