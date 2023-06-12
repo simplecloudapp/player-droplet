8cffdf23a1364e3bccedf21844c375c3b4d15999
Philipp <p.eistrach@gmail.com>
2023-06-12 11:25:03 +0200

    feat: plugin-shared sub-module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,5 @@ include("player-api")
 include("player-server")
 include("player-shared")
 include("player-plugin")
+include("player-plugin:plugin-shared")
+findProject(":player-plugin:plugin-shared")?.name = "plugin-shared"
