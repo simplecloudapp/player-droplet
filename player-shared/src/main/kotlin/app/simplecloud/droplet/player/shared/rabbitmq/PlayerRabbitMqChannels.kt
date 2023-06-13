c61bf2c46801237439ae645930ec475fc2033a5b
Philipp <p.eistrach@gmail.com>
2023-06-13 09:10:03 +0200

    feat: default channels for rabbitmq

---
+++ player-shared/src/main/kotlin/app/simplecloud/droplet/player/shared/rabbitmq/PlayerRabbitMqChannels.kt
@@ -0,0 +1,8 @@
+package app.simplecloud.droplet.player.shared.rabbitmq
+
+object PlayerRabbitMqChannels {
+
+    const val CHANNEL_PREFIX = "droplet.player"
+    const val ADVENTURE = "$CHANNEL_PREFIX/adventure"
+
+}
