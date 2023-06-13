94213515454f32b400f1be3879eb4e53fe2d1f81
Philipp <p.eistrach@gmail.com>
2023-06-13 09:12:37 +0200

    feat: rename channels

--- player-shared/src/main/kotlin/app/simplecloud/droplet/player/shared/rabbitmq/RabbitMqChannelNames.kt
+++ player-shared/src/main/kotlin/app/simplecloud/droplet/player/shared/rabbitmq/RabbitMqChannelNames.kt
@@ -1,6 +1,6 @@
 package app.simplecloud.droplet.player.shared.rabbitmq
 
-object PlayerRabbitMqChannels {
+object RabbitMqChannelNames {
 
     const val CHANNEL_PREFIX = "droplet.player"
     const val ADVENTURE = "$CHANNEL_PREFIX/adventure"
