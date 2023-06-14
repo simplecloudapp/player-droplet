28e2edc22f3640b962d61db1431ae87f29587fff
Philipp <p.eistrach@gmail.com>
2023-06-14 18:09:33 +0200

    implement: bungeecord online counter

--- player-plugin/plugin-bungeecord/src/main/kotlin/app/simplecloud/droplet/player/plugin/bungeecord/PlayerBungeecordPlugin.kt
+++ player-plugin/plugin-bungeecord/src/main/kotlin/app/simplecloud/droplet/player/plugin/bungeecord/PlayerBungeecordPlugin.kt
@@ -3,12 +3,15 @@ package app.simplecloud.droplet.player.plugin.bungeecord
 import app.simplecloud.droplet.player.api.PlayerApiSingleton
 import app.simplecloud.droplet.player.plugin.bungeecord.listener.PlayerConnectionListener
 import app.simplecloud.droplet.player.plugin.bungeecord.listener.PlayerDisconnectListener
+import app.simplecloud.droplet.player.plugin.shared.OnlinePlayerChecker
 import app.simplecloud.droplet.player.plugin.shared.proxy.PlayerProxyApi
 import net.md_5.bungee.api.plugin.Plugin
 
 class PlayerBungeecordPlugin : Plugin() {
 
-    private val playerApi = PlayerProxyApi()
+    private val playerApi = PlayerProxyApi(
+        OnlinePlayerChecker { proxy.getPlayer(it) != null }
+    )
 
     override fun onEnable() {
         PlayerApiSingleton.init(playerApi)
