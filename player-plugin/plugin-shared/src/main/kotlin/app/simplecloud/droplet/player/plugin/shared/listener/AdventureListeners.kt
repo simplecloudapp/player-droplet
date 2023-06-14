49e3b72bd0628a2c3c607363f548689270da66c3
Philipp <p.eistrach@gmail.com>
2023-06-14 15:29:32 +0200

    feat: AdventureListeners

---
+++ player-plugin/plugin-shared/src/main/kotlin/app/simplecloud/droplet/player/plugin/shared/listener/AdventureListeners.kt
@@ -0,0 +1,33 @@
+package app.simplecloud.droplet.player.plugin.shared.listener
+
+import app.simplecloud.droplet.player.plugin.shared.repository.AudienceRepository
+import app.simplecloud.droplet.player.proto.*
+import app.simplecloud.droplet.player.shared.rabbitmq.RabbitMqListener
+import com.google.protobuf.Message
+
+object AdventureListeners {
+
+    fun all(repository: AudienceRepository): List<RabbitMqListener<out Message>> {
+        return allWithClasses(repository).map { it.second }
+    }
+
+    fun allWithClasses(repository: AudienceRepository): List<Pair<Class<out Message>, RabbitMqListener<out Message>>> {
+        return listOf(
+            SendClearTitleEvent::class.java to ClearTitleListener(repository),
+            SendBossBarHideEvent::class.java to HideBossBarListener(repository),
+            SendOpenBookEvent::class.java to OpenBookEvent(repository),
+            SendPlaySoundEvent::class.java to PlaySoundEvent(repository),
+            SendPlaySoundToCoordinatesEvent::class.java to PlaySoundToCoordinatesListener(repository),
+            SendResetTitleEvent::class.java to ResetTitleListener(repository),
+            SendActionbarEvent::class.java to SendActionbarListener(repository),
+            SendMessageEvent::class.java to SendMessageListener(repository),
+            SendPlayerListHeaderAndFooterEvent::class.java to SendPlayerListHeaderAndFooterListener(repository),
+            SendTitlePartSubTitleEvent::class.java to SendTitlePartSubTitleListener(repository),
+            SendTitlePartTimesEvent::class.java to SendTitlePartTimesListener(repository),
+            SendTitlePartTitleEvent::class.java to SendTitlePartTitleListener(repository),
+            SendBossBarEvent::class.java to ShowBossBarListener(repository),
+            SendStopSoundEvent::class.java to StopSoundListener(repository),
+        )
+    }
+
+}
