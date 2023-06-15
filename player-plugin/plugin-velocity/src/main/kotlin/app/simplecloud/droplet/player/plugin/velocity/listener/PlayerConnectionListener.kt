bc9345cadd1849177dbc4ae2816fcd5712086563
Philipp <p.eistrach@gmail.com>
2023-06-15 13:19:48 +0200

    refactor: switch velocity PostLoginEvent to LoginEvent

--- player-plugin/plugin-velocity/src/main/kotlin/app/simplecloud/droplet/player/plugin/velocity/listener/PlayerConnectionListener.kt
+++ player-plugin/plugin-velocity/src/main/kotlin/app/simplecloud/droplet/player/plugin/velocity/listener/PlayerConnectionListener.kt
@@ -3,15 +3,18 @@ package app.simplecloud.droplet.player.plugin.velocity.listener
 import app.simplecloud.droplet.player.plugin.shared.proxy.PlayerProxyApi
 import app.simplecloud.droplet.player.proto.CloudPlayerLoginRequest
 import app.simplecloud.droplet.player.proto.PlayerConnectionConfiguration
+import com.velocitypowered.api.event.PostOrder
 import com.velocitypowered.api.event.Subscribe
-import com.velocitypowered.api.event.connection.PostLoginEvent
+import com.velocitypowered.api.event.connection.LoginEvent
 
 class PlayerConnectionListener(
     private val playerApi: PlayerProxyApi
 ) {
 
-    @Subscribe
-    fun onPlayerJoin(event: PostLoginEvent) {
+    @Subscribe(order = PostOrder.EARLY)
+    fun onPlayerJoin(event: LoginEvent) {
+        if (!event.result.isAllowed) return
+
         val player = event.player
         playerApi.proxyController.handleLogin(
             CloudPlayerLoginRequest.newBuilder()
