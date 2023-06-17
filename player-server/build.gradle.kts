60390aa24df6a72d0d9f69453a0ed1bb34f56a72
EinsLucaaa <74465789+EinsLucaaa@users.noreply.github.com>
2023-06-17 16:33:47 +0200

    fix: main class

--- player-server/build.gradle.kts
+++ player-server/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    application
+}
+
 dependencies {
     api(project(":player-shared"))
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
@@ -5,4 +9,8 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
     implementation("redis.clients:jedis:4.4.1")
     implementation("dev.morphia.morphia:morphia-core:2.3.4")
+}
+
+application {
+    mainClass.set("app.simplecloud.droplet.player.server.launcher.LauncherKt")
 }
