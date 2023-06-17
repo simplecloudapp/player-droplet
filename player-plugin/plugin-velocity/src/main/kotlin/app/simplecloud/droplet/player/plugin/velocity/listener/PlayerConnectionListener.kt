bdff04bb1dd93f1aa91750906bca6c4420fd3ac0
Philipp <p.eistrach@gmail.com>
2023-06-17 11:38:31 +0200

    feat: kick player if player-server is offline

--- player-plugin/plugin-velocity/src/main/kotlin/app/simplecloud/droplet/player/plugin/velocity/listener/PlayerConnectionListener.kt
+++ player-plugin/plugin-velocity/src/main/kotlin/app/simplecloud/droplet/player/plugin/velocity/listener/PlayerConnectionListener.kt
@@ -3,16 +3,20 @@ package app.simplecloud.droplet.player.plugin.velocity.listener
 import app.simplecloud.droplet.player.plugin.shared.proxy.PlayerProxyApi
 import app.simplecloud.droplet.player.proto.CloudPlayerLoginRequest
 import app.simplecloud.droplet.player.proto.PlayerConnectionConfiguration
+import com.velocitypowered.api.event.Continuation
 import com.velocitypowered.api.event.PostOrder
+import com.velocitypowered.api.event.ResultedEvent
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.connection.LoginEvent
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
 
 class PlayerConnectionListener(
-    private val playerApi: PlayerProxyApi
+    private val playerApi: PlayerProxyApi,
 ) {
 
     @Subscribe(order = PostOrder.EARLY)
-    fun onPlayerJoin(event: LoginEvent) {
+    fun onPlayerJoin(event: LoginEvent, continuation: Continuation) {
         if (!event.result.isAllowed) return
 
         val player = event.player
@@ -29,6 +33,19 @@ class PlayerConnectionListener(
                 )
                 .build()
         )
+            .thenAccept {
+                continuation.resume()
+            }
+            .exceptionally {
+                event.result = ResultedEvent.ComponentResult.denied(
+                    Component.text(
+                        "Unable to connect player!",
+                        NamedTextColor.RED
+                    )
+                )
+                continuation.resumeWithException(it)
+                null
+            }
     }
 
 }
