2696eaa7263e6060a4c0fe0665253fb3936679e0
EinsLucaaa <74465789+EinsLucaaa@users.noreply.github.com>
2024-10-05 11:53:24 +0200

    update: minestom dependency

--- player-plugin/plugin-minestom/build.gradle.kts
+++ player-plugin/plugin-minestom/build.gradle.kts
@@ -1,5 +1,5 @@
 dependencies {
-    compileOnly("com.github.Minestom.Minestom:Minestom:954e8b3915")
+    compileOnly("net.minestom:minestom-snapshots:59406d5b54")
     api(project(":player-shared"))
     api(project(":player-plugin:plugin-shared"))
     api(project(":player-api"))
